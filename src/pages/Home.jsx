d7d7e45f9564fd609392b1962f166cdecf293079
suyashdhumal35 <suyashdhumal35@gmail.com>
2025-06-11 17:32:41 +0530

    lazyyy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
-import Banner from "./homepage/Banner"
+import { lazy, Suspense } from 'react';
 import Marquee from "./homepage/Marque"
+
+// Lazy load the Banner component
+const Banner = lazy(() => import("./homepage/Banner"));
 
 const Home = () => {
   return (
     <div>
-      <Banner/>
+      <Suspense fallback={<div>Loading banner...</div>}>
+        <Banner/>
+      </Suspense>
       <Marquee/>
     </div>
   )
